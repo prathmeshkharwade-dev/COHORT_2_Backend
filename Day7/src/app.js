ce9a3c235a653054ec729a87e452599751f2697d
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-02-04 03:50:34 +0530

    add day7

---
+++ Day7/src/app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const noteModel = require("./models/notes.model")
+const app = express() 
+
+app.use(express.json())
+
+app.post("/notes" , async (req ,res) => {
+    const{title,description, } = req.body
+
+    const note = await noteModel.create({
+        title, description, 
+    })
+
+    res.status(201).json({
+        message:"Note created SuccessFully",
+        note
+    })
+})
+
+app.get("/notes" , async (req, res) => {
+     const notes = await  noteModel.find()
+
+     res.status(200).json({
+        message:"Notes fecthed SuccessFully",
+        notes
+    })
+})
+
+module.exports = app 
