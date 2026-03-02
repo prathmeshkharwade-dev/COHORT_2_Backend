e0fc539491e92314b3b63ecf679bf0911fa4abed
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-03-03 03:08:55 +0530

    feat: Implement authentication flow with registration and login
    - Added AuthContext for managing user state and loading status.
    - Created Register and Login pages with form handling.
    - Implemented Protected component to guard routes based on authentication.
    - Developed custom hooks for authentication logic (useAuth).
    - Integrated axios for API calls to handle user registration, login, and fetching user data.
    - Styled components for registration and login pages.
    - Set up routing for authentication-related pages.
    - Added global styles and button styles for consistent UI.

--- Moodify Day25/Frontend/src/features/Expression/components/FaceExpression.jsx
+++ Moodify Day25/Frontend/src/features/Expression/components/FaceExpression.jsx
@@ -1,23 +1,18 @@
 import { useEffect, useRef, useState } from "react";
-import { detect,init } from "../Utils/utils";
+import { detect,init } from "../utils/utils";
 
 
 export default function FaceExpression() {
     const videoRef = useRef(null);
     const landmarkerRef = useRef(null);
-    const streamRef = useRef(null)
+    const streamRef = useRef(null);
 
     const [ expression, setExpression ] = useState("Detecting...");
 
-    
-
     useEffect(() => {
-        
-
         init({landmarkerRef,videoRef,streamRef});
 
         return () => {
-
             if (landmarkerRef.current) {
                 landmarkerRef.current.close();
             }
@@ -38,7 +33,7 @@ export default function FaceExpression() {
                 playsInline
             />
             <h2>{expression}</h2>
-            <button onClick={()=>{detect({landmarkerRef,videoRef,setExpression})}}> Detect expression</button>
+            <button onClick={()=>{detect({landmarkerRef,videoRef,setExpression})}} >Detect expression</button>
         </div>
     );
 }
